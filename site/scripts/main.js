35911da24a1e1b399d9b8ca7e87bf12c2e005daa
Eyal Gershon <eyal@way2cu.com>
2017-07-24 18:49:52 +0300

    Added script to inject source into hidden field

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -51,6 +51,27 @@ Site.is_mobile = function() {
 Site.on_load = function() {
 	if (Site.is_mobile())
 		Site.mobile_menu = new Caracal.MobileMenu();
+	// script for dynamic source insertion into hidden field
+		var sourcefield = document.querySelectorAll("input[name='source']");
+		var source = null;
+		var is_google = document.referrer.indexOf('google') > -1;
+		var is_facebook = document.referrer.indexOf('facebook') > -1;
+		var has_gclid = window.location.search.indexOf('gclid') > -1;
+
+		// assign source according to conditions
+		if (has_gclid)
+		    source = 'Adwords';
+		else if (is_google && !has_gclid)
+		    source = 'Organic';
+		else if (is_facebook)
+		    source = 'Facebook';
+		else
+		    source = 'Direct';
+
+		for (i = 0; i < sourcefield.length; i++){
+			sourcefield[i].value = source;
+		}
+
 	// handle analytics event
 		var dataLayer = window.dataLayer || new Array();
 		for (var i=0, count=Caracal.ContactForm.list.length; i<count; i++)
